11bd34eb8b1eef52a87d1412b6ccfe5be5081587
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-25 19:42:51 -0300

    feat: criando aplicação fron-end GiFind parte 2

--- FRONT-END/gitfind/src/pages/home/index.js
+++ FRONT-END/gitfind/src/pages/home/index.js
@@ -1,9 +1,20 @@
 import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <div className="conteudo">
+          <img src={background} className="backgroud" alt=""/>
+          <div className="info">
+            <div>
+              <input type="text" name="usuario" placeholder="@username" readonly/>
+              <button>Buscar</button>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
